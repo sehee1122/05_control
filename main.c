4d385b366337e3f02f2488e9bfd61a40dacf988c
sehee1122 <55127603+sehee1122@users.noreply.github.com>
2019-10-08 21:32:13 +0900

    05-2_Project

--- main.c
+++ main.c
@@ -37,15 +37,75 @@ int main(int argc, char *argv[]) {
 	scanf("%d", &input3);
 	
 	if (input3 > 0) {
-		printf("positive\n");
+		printf("positive\n\n");
 	}
 		
 	else if (input3 < 0) {
-		printf("negative\n");
+		printf("negative\n\n");
 	}
 	
 	else
-		printf("zero\n");	
+		printf("zero\n\n");
 	
+	// Practice 05-4
+	
+	int i;
+	int meter; 
+	
+	while (i < 3) {
+		meter = i * 1609;
+		printf("%d 마일은 %d 미터입니다.\n", i, meter);
+		i++;
+	}
+	
+	printf("\n");
+		
+	// Practice 05-5
+	
+	int k;
+	for (k = 0; k < 10; k++)
+		printf("Hello World!\n");
+	
+	printf("\n");
+	
+	// Practice 05-6
+	
+	int num = 0;
+	char c;
+	
+	printf("input a string: ");
+
+	while ((c = getchar()) != '\n') {
+		if ( c >= '0' && c <= '9') {
+			num++;
+		}
+	}
+	
+	printf("the number of digits is %d\n\n", num);
+	
+	// Practice 05-7	
+	
+	int m;
+	int n = 1;
+	int answer = 59;
+	
+	printf("Guess a number :");
+	scanf("%d", &m);
+
+	do {
+		if (m > answer) {
+			printf("high!\nGuess a number :");
+			scanf("%d", &m);
+		}
+		
+		else {
+			printf("low!\nGuess a number :");
+			scanf("%d", &m);
+		}
+		n++;
+	} while (m < answer || m > answer);
+
+		printf("Congratulation! trails:%d\n", n);
+
 	return 0;
 }
